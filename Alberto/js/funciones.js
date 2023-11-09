426df4962862c2b10f110929714290f24bbe3982
Alberto <albertosaboya@gmail.com>
2023-11-09 10:37:02 +0100

    avanzReq1

--- Alberto/js/funciones.js
+++ Alberto/js/funciones.js
@@ -1,48 +1,52 @@
-const URL_DESTINO = "http://localhost:5500/AE-2.-AJAX"
-const RECURSO = "tamanios.json"
+const URL_DESTINO = "http://localhost:5500/AE-2.-AJAX/Alberto/"
+const RECURSO = "tamanios_ingredientes.json"
 
-    function enviarPeticionAsincrona() {
+function enviarpeticionAjax() {
+    let xmlHttp = new XMLHttpRequest()
+    xmlHttp.open('GET', URL_DESTINO + RECURSO, true)//Asincrono
+    xmlHttp.send() //podemos poner null o no, es lo mismo
 
-        let xmlHttp = new XMLHttpRequest()
-
-        xmlHttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    procesarRespuesta(this.responseText)//Obtenemos el valor en texto
-                } else {
-                    alert("ZASCA!")
-                }
-            }
-        }
-
-        xmlHttp.open('GET', URL_DESTINO + RECURSO, true)
-        xmlHttp.send(null)
+    //esta funcion de callback se ejecutara cuando se haya procesado la respueta HTTP
+    xmlHttp.onload = function () {
+        procesarRespuesta(this.responseText)
     }
 
-    function procesarRespuesta(jsonDoc) {
-        //Convertimos un texto a un objeto JSON
-        var objetoJson = JSON.parse(jsonDoc)
-        //Podemos hacer lo contrario con "JSON.stringify(obj)"
-        console.log(objetoJson)
-
-        //Lo suyo seria crear objetos con el DOM, esto esta regulín
-        //pero como ejemplo sencillo vale
-        var table = "<tr><th>Title</th><th>Artist</th></tr>";
-        var arrayCDs = objetoJson.CATALOG.CD;//Ojo mayusculas y minusculas, es como este en el json original
-        
-        //Iteramos el array de CDs y formamos las filas y columnas
-        for(let cd of arrayCDs){
-            table += "<tr><td>" + cd.TITLE + "</td>" + 
-                "<td>" + cd.ARTIST + "</td></tr>";
-        }
-
-        resultadoBusqueda.innerHTML = table;
-
-        /*Tambien podemos hacerlo así, con un for clasico
-        for (let i = 0; i < arrayCDs.length; i++) {
-            table += "<tr><td>" + arrayCDs[i].TITLE + "</td>" + 
-                "<td>" + arrayCDs[i].ARTIST + "</td></tr>";
-        }
-        resultadoBusqueda.innerHTML = table;
-        */
+    xmlHttp.onerror = function () {
+        alert("No se pueden cargar los datos!")
     }
+
+}
+
+function procesarRespuesta(jsonDoc) {
+    //Convertimos un texto a un objeto JSON
+    var objetoJson = JSON.parse(jsonDoc)
+    //Podemos hacer lo contrario con "JSON.stringify(obj)"
+    console.log(objetoJson)
+
+    var table = "<tr><th>Tamanio</th></tr>";
+    var arraytamanios = objetoJson.TAMANIOS;
+    var arrayingredientes = objetoJson.INGREDIENTES;
+
+
+    // Mostrar tamaños de pizza como botones de radio
+    var conTam = document.getElementById("tamanios");
+    var tamDiv = document.createElement("div");
+    tamDiv.innerHTML = "Elige un tamaño de pizza <br/>";
+
+    for (let i = 0; i < arrayTam.length; i++) {
+        var radioB = document.createElement("input");
+        radioB.type = "radio";
+        radioB.name = "tamañoPizza";
+        radioB.id = arrayTam[i].nombre;
+        radioB.value = arrayTam[i].nombre;
+
+        var radioL = document.createElement("label");
+        radioL.textContent = arrayTam[i].nombre;
+
+        tamDiv.appendChild(radioB);
+        tamDiv.appendChild(radioL);
+    }
+
+    conTam.appendChild(tamDiv);
+
+}
